f50e0f257d5b15445b02bf248462c62b5176c100
Didi <dz260205@gmail.com>
2023-09-22 10:54:24 +0400

    Prog V1

---
+++ V_1/1/1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ded.Visible= true;
+        }
+
+        private void ded_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}   
