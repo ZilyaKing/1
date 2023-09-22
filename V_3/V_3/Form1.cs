6e14c607cc84b4075521f4c38995ba5caa187771
Didi <dz260205@gmail.com>
2023-09-22 10:57:42 +0400

    ProgV3

---
+++ V_3/V_3/Form1.cs
@@ -0,0 +1,25 @@
+namespace V_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox3.Visible = true;
+        }
+    }
+}
